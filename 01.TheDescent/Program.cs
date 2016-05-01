7c51b258c60b70763ee2b0bafc3f55a48ddc4d4f
Anh <h.anh.viet91@gmail.com>
2016-05-01 15:46:30 +0300

    The Descent checkin

---
+++ 01.TheDescent/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // game loop
+        while (true)
+        {
+            int highestSoFar = 0;
+            int mountToShoot = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                int mountainH = int.Parse(Console.ReadLine()); // represents the height of one mountain, from 9 to 0.
+
+                if (highestSoFar < mountainH)
+                {
+                    highestSoFar = mountainH;
+                    mountToShoot = i;
+                }
+            }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine(mountToShoot); // The number of the mountain to fire on.
+        }
+    }
+}
