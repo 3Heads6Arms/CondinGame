8706e10cbaa31a9972225e82ca2752bd018a0392
Anh <h.anh.viet91@gmail.com>
2016-05-01 16:28:18 +0300

    Added new project

---
+++ 05.Temperatures/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine()); // the number of temperatures to analyse
+        var temps = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        var minTemp = temps
+            .Select(int.Parse)
+            .OrderBy(Math.Abs)
+            .ThenByDescending(x => x)
+            .FirstOrDefault();
+
+        Console.WriteLine(minTemp);
+    }
+}
