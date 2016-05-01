e3f91b68f48209f849ce29e08d44d68963cc012c
Anh <h.anh.viet91@gmail.com>
2016-05-01 21:47:57 +0300

    Finished ASCII Art

---
+++ 06.ASCIIArt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int L = int.Parse(Console.ReadLine());
+        int H = int.Parse(Console.ReadLine());
+        var T = Console.ReadLine().ToUpper().ToCharArray();
+
+        for (int i = 0; i < H; i++)
+        {
+            string ROW = Console.ReadLine();
+
+            var rowResults = T
+                .Select(x => char.IsLetter(x) ? (x - 'A') * L : 26 * L)
+                .Select(x => ROW.Substring(x, L));
+            Console.WriteLine(string.Join(string.Empty, rowResults));
+        }
+    }
+}
